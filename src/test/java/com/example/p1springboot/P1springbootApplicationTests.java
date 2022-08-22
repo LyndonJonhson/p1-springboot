43c894c0d344146e234166f6e949977fe0331694
Lyndon Jonhson <jonhsoncabral13@gmail.com>
2022-08-22 19:04:18 -0300

    Project created

---
+++ src/test/java/com/example/p1springboot/P1springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1springbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
